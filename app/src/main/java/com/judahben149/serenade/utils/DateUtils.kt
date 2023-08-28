807b69b920ae7794e4ab86ab321caeff751716b5
Judah <judahben149@gmail.com>
2023-08-28 20:16:32 +0100

    Created function to get greeting

---
+++ app/src/main/java/com/judahben149/serenade/utils/DateUtils.kt
@@ -0,0 +1,15 @@
+package com.judahben149.serenade.utils
+
+import java.util.Calendar
+
+fun getGreeting(): String {
+    val currentTime = Calendar.getInstance().apply {
+        timeInMillis = System.currentTimeMillis()
+    }
+
+    return when(currentTime.get(Calendar.HOUR_OF_DAY)) {
+        in 0..11 -> "Good morning"
+        in 12..16 -> "Good afternoon"
+        else -> "Good evening"
+    }
+}
