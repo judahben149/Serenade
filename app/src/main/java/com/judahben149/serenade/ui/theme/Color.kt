0a133d6b8359b269708d31b1d8e6e968ba0d34b5
Judah <judahben149@gmail.com>
2023-08-29 18:31:13 +0100

    Updated app colors

--- app/src/main/java/com/judahben149/serenade/ui/theme/Color.kt
+++ app/src/main/java/com/judahben149/serenade/ui/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// App colors
+val RedCrayola = Color(0xFFE84855)
+val RedCrayolaAlpha40 = Color(0x66E84855)
+val black = Color(0xFF191414)
+val white = Color(0xFFFFFFFF)
