5bed78e5e8a0796c82c66eb645f1f87f90ca7128
Judah <judahben149@gmail.com>
2023-08-29 19:09:29 +0100

    Updated Main Activity

--- app/src/main/java/com/judahben149/serenade/ui/MainActivity.kt
+++ app/src/main/java/com/judahben149/serenade/ui/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.judahben149.serenade.ui.navigation.Navigation
 import com.judahben149.serenade.ui.theme.SerenadeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,30 +21,23 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SerenadeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    Navigation(navHostController = navController)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SerenadeTheme {
-        Greeting("Android")
+
     }
 }
