61aee66473b3f61982aebd7cf3bada2ba26e46af
Judah <judahben149@gmail.com>
2023-08-30 21:17:40 +0100

    Implemented Home Screen to fetch and display local media content from in list

--- app/src/main/java/com/judahben149/serenade/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/judahben149/serenade/ui/screens/home/HomeScreen.kt
@@ -1,10 +1,16 @@
 package com.judahben149.serenade.ui.screens.home
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -12,6 +18,7 @@ import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -19,6 +26,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -28,10 +36,12 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.judahben149.serenade.ui.components.ChipComponent
 import com.judahben149.serenade.ui.components.SnackBarComponent
+import com.judahben149.serenade.ui.components.TrackListItemComponent
 
 @Composable
 fun HomeScreen(navController: NavHostController, homeViewModel: HomeViewModel = hiltViewModel()) {
     val scope = rememberCoroutineScope()
+    val context = LocalContext.current
     val state by homeViewModel.state.collectAsState()
     val snackBarHostState = remember { SnackbarHostState() }
 
@@ -41,6 +51,28 @@ fun HomeScreen(navController: NavHostController, homeViewModel: HomeViewModel = 
 
     ) { paddingValues ->
 
+        if (state.isLoading) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+            }
+        } else {
+
+            LazyColumn(
+                modifier = Modifier
+                    .padding(
+                        top = paddingValues.calculateTopPadding(),
+                        bottom = paddingValues.calculateBottomPadding()
+                    ),
+                verticalArrangement = Arrangement.spacedBy(4.dp),
+                contentPadding = PaddingValues(horizontal = 12.dp)
+            ) {
+                items(state.trackList.size) {
+                    TrackListItemComponent(track = state.trackList[it]) {
+                        Toast.makeText(context, "Duration - $it", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -61,13 +93,15 @@ fun TopAppBarComponent(viewModel: HomeViewModel, state: HomeUIState) {
                 ChipComponent(
                     text = "Music",
                     isSelected = state.trackType == TrackType.MUSIC,
-                    onClick = { viewModel.updateTrackType(TrackType.MUSIC) }
+                    onClick = {
+                        viewModel.updateTrackType(TrackType.MUSIC)
+                    }
                 )
                 ChipComponent(
                     text = "Podcasts & Shows",
                     isSelected = state.trackType == TrackType.PODCASTS,
                     onClick = { viewModel.updateTrackType(TrackType.PODCASTS) }
-                    )
+                )
             }
         },
         actions = {
--- app/src/main/java/com/judahben149/serenade/ui/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/judahben149/serenade/ui/screens/home/HomeViewModel.kt
@@ -1,25 +1,89 @@
 package com.judahben149.serenade.ui.screens.home
 
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.judahben149.serenade.domain.models.Track
+import com.judahben149.serenade.utils.MusicContentHelper
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(): ViewModel() {
+class HomeViewModel @Inject constructor(
+    private val musicContentHelper: MusicContentHelper,
+) : ViewModel() {
 
     private val _state = MutableStateFlow(HomeUIState())
     val state = _state.asStateFlow()
 
+    init {
+        getAllTracks()
+    }
+
     fun updateTrackType(trackType: TrackType) {
         _state.update { it.copy(trackType = trackType) }
+    }
+
+    private fun getAllTracks() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val trackList = musicContentHelper.retrieveAllTracks()
+
+            if (trackList.size > 0) {
+                var actualTrackList: ArrayList<Track> = arrayListOf()
+
+                for (track in trackList) {
+                    val albumArt = getAlbumArt(track.contentUri)
+
+                    val trackDetails = Track(
+                        id = track.id,
+                        trackName = track.name ?: "Empty name",
+                        artistName = track.artist ?: "Empty Artist",
+                        duration = track.duration,
+                        albumArt = albumArt
+                    )
+
+                    actualTrackList.add(trackDetails)
+                }
+
+                withContext(Dispatchers.Main) {
+                    _state.update {
+                        it.copy(
+                            isTrackDataFetched = true,
+                            trackList = actualTrackList,
+                            isLoading = false
+                        )
+                    }
+                }
+
+            } else {
+                _state.update { it.copy(isLoading = false, isTrackDataFetched = true) }
+            }
+        }
+    }
+
+    private suspend fun getAlbumArt(uri: Uri): Bitmap? {
+        return musicContentHelper.getAlbumArtThumbnail(uri)
+    }
+
+    fun toggleLoading(isLoading: Boolean) {
+        _state.update { it.copy(isLoading = isLoading) }
     }
 }
 
 data class HomeUIState(
-    val trackType: TrackType = TrackType.ALL
+    val isLoading: Boolean = true,
+    val isTrackDataFetched: Boolean = false,
+    val trackList: List<Track> = emptyList(),
+    val trackType: TrackType = TrackType.ALL,
 )
 
 enum class TrackType {
