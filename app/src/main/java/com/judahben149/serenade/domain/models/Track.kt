652932b7b3cede3eb215d7cf3468d416a5d12a34
Judah <judahben149@gmail.com>
2023-08-30 21:17:59 +0100

    Updated Track model

---
+++ app/src/main/java/com/judahben149/serenade/domain/models/Track.kt
@@ -0,0 +1,13 @@
+package com.judahben149.serenade.domain.models
+
+import android.graphics.Bitmap
+
+data class Track(
+    val id: Long = -1L,
+    val trackName: String = "",
+    val artistName: String = "",
+    val thumbnail: String = "",
+    val duration: Int = 0,
+    val contentUri: String = "",
+    val albumArt: Bitmap? = null
+)
