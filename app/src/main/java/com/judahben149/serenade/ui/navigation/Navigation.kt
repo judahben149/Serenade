2f4b6abdba6bd052b0f230b5823d4f5a7c647415
Judah <judahben149@gmail.com>
2023-08-31 15:58:23 +0100

    Added Detail composable to Navigation graph

--- app/src/main/java/com/judahben149/serenade/ui/navigation/Navigation.kt
+++ app/src/main/java/com/judahben149/serenade/ui/navigation/Navigation.kt
@@ -5,18 +5,24 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.judahben149.serenade.ui.screens.home.HomeScreen
+import com.judahben149.serenade.ui.screens.trackDetail.TrackDetailScreen
 
 @Composable
 fun Navigation(navHostController: NavHostController) {
 
     NavHost(navController = navHostController, startDestination = Screens.HomeScreen.route) {
 
-        composable(
-            route = Screens.HomeScreen.route
-        ) {
+        composable(route = Screens.HomeScreen.route) {
             HomeScreen(navController = navHostController)
         }
+        
+        composable(route = Screens.TrackDetailScreen.route) {
+            val trackId = it.arguments?.getString("trackId")
 
+            trackId?.let { id ->
+                TrackDetailScreen(navController = navHostController, trackId = id.toLong())
+            }
+        }
 
     }
 }
