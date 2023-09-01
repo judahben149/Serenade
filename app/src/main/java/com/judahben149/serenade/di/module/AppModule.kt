3797742ce20e83edd4b3ed7375279754a03feb65
Judah <judahben149@gmail.com>
2023-09-01 17:16:13 +0100

    Provided Exoplayer components via Hilt module

--- app/src/main/java/com/judahben149/serenade/di/module/AppModule.kt
+++ app/src/main/java/com/judahben149/serenade/di/module/AppModule.kt
@@ -1,6 +1,9 @@
 package com.judahben149.serenade.di.module
 
 import android.content.Context
+import androidx.media3.common.Player
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.MediaSession
 import com.judahben149.serenade.utils.MusicContentHelper
 import dagger.Module
 import dagger.Provides
@@ -23,5 +26,19 @@ object AppModule {
     @Singleton
     fun providesMusicContentHelper(context: Context): MusicContentHelper {
         return MusicContentHelper(context)
+    }
+
+    @Provides
+    @Singleton
+    fun providesExoPlayer(context: Context): Player {
+        return ExoPlayer.Builder(context).apply {
+            setHandleAudioBecomingNoisy(true)
+        }.build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesMediaSession(context: Context, player: Player): MediaSession {
+        return MediaSession.Builder(context, player).build()
     }
 }
