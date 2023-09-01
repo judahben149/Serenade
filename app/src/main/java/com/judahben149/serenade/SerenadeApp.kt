005e4b1747a2c8acf82b06b9967c97bd1977d829
Judah <judahben149@gmail.com>
2023-09-01 16:59:02 +0100

    Setup Timber in app

--- app/src/main/java/com/judahben149/serenade/SerenadeApp.kt
+++ app/src/main/java/com/judahben149/serenade/SerenadeApp.kt
@@ -2,6 +2,13 @@ package com.judahben149.serenade
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class SerenadeApp: Application()
+class SerenadeApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
