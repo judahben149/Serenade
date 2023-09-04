85f2ed1bf09389635cd201ab59d0628a4cc679b5
Judah <judahben149@gmail.com>
2023-09-04 22:17:52 +0100

    Created pref module to provide shared preferences via DI

---
+++ app/src/main/java/com/judahben149/serenade/di/module/PreferencesModule.kt
@@ -0,0 +1,28 @@
+package com.judahben149.serenade.di.module
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.judahben149.serenade.utils.Constants
+import com.judahben149.serenade.utils.PrefUtils
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object PreferencesModule {
+
+    @Provides
+    @Singleton
+    fun providesSharedPreferences(context: Context): SharedPreferences {
+        return context.getSharedPreferences(Constants.SERENADE_PREFS, Context.MODE_PRIVATE)
+    }
+
+    @Provides
+    @Singleton
+    fun providesPrefUtils(): PrefUtils {
+        return PrefUtils()
+    }
+}
