4d76b873806e77f4e4ec03b000495dcd65f961a0
Judah <judahben149@gmail.com>
2023-09-08 15:45:20 +0100

    Provided Gson and SessionManager dependencies via Hilt

--- app/src/main/java/com/judahben149/serenade/di/module/AppModule.kt
+++ app/src/main/java/com/judahben149/serenade/di/module/AppModule.kt
@@ -4,13 +4,22 @@ import android.content.Context
 import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.MediaSession
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import com.judahben149.serenade.domain.SerenadePlayer
-import com.judahben149.serenade.utils.MusicContentHelper
+import com.judahben149.serenade.domain.SessionManager
+import com.judahben149.serenade.domain.models.SerenadeColor
+import com.judahben149.serenade.domain.models.SerenadeSwatch
+import com.judahben149.serenade.utils.appUtils.PrefUtils
+import com.judahben149.serenade.utils.resourceUtils.MusicContentHelper
+import com.judahben149.serenade.utils.serializationUtils.SerenadeColorSerializer
+import com.judahben149.serenade.utils.serializationUtils.SerenadeSwatchSerializer
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
@@ -47,5 +56,21 @@ object AppModule {
     @Singleton
     fun providesSerenadePlayer(player: Player): SerenadePlayer {
         return SerenadePlayer(player)
+    }
+
+    @Provides
+    @Singleton
+    @Named("serenade_gson")
+    fun providesGson(): Gson {
+        return GsonBuilder()
+            .registerTypeAdapter(SerenadeSwatch::class.java, SerenadeSwatchSerializer())
+            .registerTypeAdapter(SerenadeColor::class.java, SerenadeColorSerializer())
+            .create()
+    }
+
+    @Provides
+    @Singleton
+    fun providesSessionManager(prefUtils: PrefUtils): SessionManager {
+        return SessionManager(prefUtils)
     }
 }
