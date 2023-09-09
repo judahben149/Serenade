1f90b845ea7666ccf2fd2e478f0d98e0893c4988
Judah <judahben149@gmail.com>
2023-09-09 19:35:34 +0100

    Added repository functions to access database

--- app/src/main/java/com/judahben149/serenade/data/repository/TrackRepositoryImpl.kt
+++ app/src/main/java/com/judahben149/serenade/data/repository/TrackRepositoryImpl.kt
@@ -3,21 +3,34 @@ package com.judahben149.serenade.data.repository
 import android.content.Context
 import android.graphics.Bitmap
 import android.net.Uri
+import com.judahben149.serenade.data.sources.local.SerenadeDatabase
+import com.judahben149.serenade.data.sources.local.entity.TrackEntity
 import com.judahben149.serenade.domain.TrackRepository
-import com.judahben149.serenade.utils.MusicContentHelper
+import com.judahben149.serenade.utils.resourceUtils.MusicContentHelper
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 
 class TrackRepositoryImpl @Inject constructor(
     private val context: Context,
-    private val musicContentHelper: MusicContentHelper
-): TrackRepository {
+    private val database: SerenadeDatabase,
+    private val musicContentHelper: MusicContentHelper,
+) : TrackRepository {
 
     override suspend fun loadCoverBitmap(uri: Uri): Bitmap? {
         return withContext(Dispatchers.IO) {
             musicContentHelper.getAlbumArt(context, uri)
         }
+    }
+
+    override suspend fun saveTracks(trackEntity: TrackEntity) {
+        database.trackDao().saveTracks(trackEntity)
+    }
+
+    override suspend fun getAllTracks(): Flow<List<TrackEntity>> {
+        return database.trackDao().getAllTracks()
     }
 }
--- app/src/main/java/com/judahben149/serenade/domain/TrackRepository.kt
+++ app/src/main/java/com/judahben149/serenade/domain/TrackRepository.kt
@@ -1,10 +1,15 @@
 package com.judahben149.serenade.domain
 
-import android.content.Context
 import android.graphics.Bitmap
 import android.net.Uri
+import com.judahben149.serenade.data.sources.local.entity.TrackEntity
+import kotlinx.coroutines.flow.Flow
 
 interface TrackRepository {
 
     suspend fun loadCoverBitmap(uri: Uri): Bitmap?
+
+    suspend fun saveTracks(trackEntity: TrackEntity)
+
+    suspend fun getAllTracks(): Flow<List<TrackEntity>>
 }
