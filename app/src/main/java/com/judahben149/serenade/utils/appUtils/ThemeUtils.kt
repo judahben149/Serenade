e5f9ef2e93e6c952d9ddd34dabed31f28d35202c
Judah <judahben149@gmail.com>
2023-09-15 20:51:29 +0100

    refactored theme utils

--- app/src/main/java/com/judahben149/serenade/utils/appUtils/ThemeUtils.kt
+++ app/src/main/java/com/judahben149/serenade/utils/appUtils/ThemeUtils.kt
@@ -1,8 +1,9 @@
 package com.judahben149.serenade.utils.appUtils
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 
 @Composable
 fun isAppInDarkMode(): Boolean {
-    return true
+    return isSystemInDarkTheme()
 }
