eb56f5037112dd4abdf788ead4f13f866c4e8fa6
Judah <judahben149@gmail.com>
2023-09-17 03:18:17 +0100

    Refactored screens class

--- app/src/main/java/com/judahben149/serenade/ui/navigation/Screens.kt
+++ app/src/main/java/com/judahben149/serenade/ui/navigation/Screens.kt
@@ -1,6 +1,5 @@
 package com.judahben149.serenade.ui.navigation
 
 sealed class Screens(val route: String) {
-    object HomeScreen: Screens("home_screen")
-    object TrackDetailScreen: Screens("track_detail_screen/{trackId}/{trackContentUri}")
+    object Home: Screens("home")
 }
