301d5dfe5f201cd94e1be7cb1b8c2916f07d906c
Judah <judahben149@gmail.com>
2023-09-20 22:45:37 +0100

    Fixed NavGraph

--- app/src/main/java/com/judahben149/serenade/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/judahben149/serenade/ui/navigation/NavGraph.kt
@@ -6,20 +6,30 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.judahben149.serenade.ui.screens.serenadeHomeScreen.SerenadeHome
 import com.judahben149.serenade.ui.screens.serenadeHomeScreen.SerenadeHomeScreenViewModel
+import com.judahben149.serenade.ui.screens.trackDetail.NowPlayingScreen
 
 @Composable
 fun NavGraph(
-    navHostController: NavHostController,
-    viewModel: SerenadeHomeScreenViewModel
+    navController: NavHostController,
+    viewModel: SerenadeHomeScreenViewModel,
 ) {
 
     NavHost(
-        navController = navHostController,
+        navController = navController,
         startDestination = Screens.Home.route
     ) {
 
         composable(route = Screens.Home.route) {
-            SerenadeHome(viewModel)
+            SerenadeHome(
+                viewModel = viewModel,
+                onNavigateToNowPlaying = {
+                    navController.navigate(Screens.NowPlaying.route)
+                }
+            )
+        }
+
+        composable(route = Screens.NowPlaying.route) {
+            NowPlayingScreen(viewModel)
         }
     }
 }
