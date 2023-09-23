1d8a7897ea07868033234ff1781ad0c86b96387f
Judah <judahben149@gmail.com>
2023-09-23 21:47:30 +0100

    removed bottom screen item class

--- app/src/main/java/com/judahben149/serenade/ui/navigation/bottomNavigation/BottomNavScreens.kt
+++
@@ -1,9 +0,0 @@
-package com.judahben149.serenade.ui.navigation.bottomNavigation
-
-
-sealed class BottomNavScreens(val route: String) {
-    object Home: BottomNavScreens("home_nav")
-    object Library: BottomNavScreens("library_nav")
-    object Local: BottomNavScreens("local_nav")
-    object Settings: BottomNavScreens("settings_nav")
-}
